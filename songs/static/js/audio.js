232e572c5e11af98cc9615c8e20021f582d66e4f
AngelNight <tatyana.afonina@gmail.com>
2013-10-27 02:32:08 +0400

    Добавление в свои записи

--- songs/static/js/audio.js
+++ songs/static/js/audio.js
@@ -12,16 +12,26 @@
             var res = [];
             for (var i = 0; i < list.length; i++) {
                 var audio = list[i];
-               console.log(audio);
-                res.push( '<table><tr><td> <div class="favourite " id="' + audio.aid + '" owner="' + audio.owner_id + '" /> </td><td><div class="ui360"><a href="'+audio.url+'">'+audio.name+'</a></div></td><td> <input class="remove_button_style" type="image" src="/images/ic_action_remove.png" onclick="deleteSong(audio.url)"> </td></tr></table>');
+                res.push( '<table class="song_container" song="' + audio.id + '" owner="' + audio.owner + '"><tr><td> <div class="active-icon favourite "/> </td><td><div class="ui360"><a href="'+audio.url+'">'+audio.name+'</a></div></td><td> <div class="active-icon add-to-my"/> </td></tr></table>');
             }
             var container = $('#audio_list');
             container.html(res.join(''));
             container.find('.favourite').click(function(){
-               if(!$(this).hasClass('star-active')){
-                  $(this).addClass('star-active');
+               if(!$(this).hasClass('active')){
+                  $(this).addClass('active');
             //request
                 //   httpGet('/mood/'+ $(event.target).attr('my-number'));
+               }
+            });
+            container.find('.add-to-my').click(function(){
+               if(!$(this).hasClass('active')){
+                  $(this).addClass('active');
+                  var parent = $(this).closest('.song_container');
+                  VK.Api.call('audio.add', {
+                     "audio_id": parent.attr("song"),
+                     "owner_id": parent.attr("owner")
+                  }, function (r) {
+                  });
                }
             });
         }
