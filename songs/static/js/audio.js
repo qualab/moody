cc8fe974e7eee2e845e8f603e5ef3cc80d0e9b11
AngelNight <tatyana.afonina@gmail.com>
2013-10-27 02:50:15 +0400

    Рассказать друзьям

--- songs/static/js/audio.js
+++ songs/static/js/audio.js
@@ -12,7 +12,7 @@
             var res = [];
             for (var i = 0; i < list.length; i++) {
                 var audio = list[i];
-                res.push( '<table class="song_container" song="' + audio.id + '" owner="' + audio.owner + '"><tr><td> <div class="active-icon favourite "/> </td><td><div class="ui360"><a href="'+audio.url+'">'+audio.name+'</a></div></td><td> <div class="active-icon add-to-my"/> </td></tr></table>');
+                res.push( '<table class="song_container" song="' + audio.id + '" owner="' + audio.owner + '"><tr><td> <div title="Отметить подходящей по настроению" class="active-icon favourite "/> </td><td><div class="ui360"><a href="'+audio.url+'">'+audio.name+'</a></div></td><td> <div class="active-icon add-to-my" title="Добавить в мои аудиозаписи"/> </td><td> <div class="active-icon to-friends" title="Рассказать друзьям"/> </td></tr></table>');
             }
             var container = $('#audio_list');
             container.html(res.join(''));
@@ -32,6 +32,18 @@
                      "owner_id": parent.attr("owner")
                   }, function (r) {
                   });
+               }
+            });
+            container.find('.to-friends').click(function(){
+               if(!$(this).hasClass('active')){
+                  var parent = $(this).closest('.song_container');
+                  VK.Api.call('wall.post', {
+                     "friends_only": 1,
+                     "message": "Эта песня подошла мне под настроение. Найти помогло приложение MoodMusic",
+                     "attachments": ["audio", parent.attr("owner"), "_", parent.attr("song")].join('')
+                  }, function (r) {
+                  });
+                  $(this).hide();
                }
             });
         }
