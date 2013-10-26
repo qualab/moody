db1e08a0a6946f912adbc8bb3a2a73a0db106067
ansmirnov <mail@ansmirnov.ru>
2013-10-26 16:12:25 +0400

    sdf

--- songs/views.py
+++ songs/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from songs.models import Account, Mood, Song, Selection
 
 def home(request):
-    return render(request, 'index.html', {
+    return render(request, 'anonimous.html', {
 #        'account':   Account.objects.get(pk=1),
 #        'song':      Song.objects.get(pk=1),
     })
